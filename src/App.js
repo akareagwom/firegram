2414cd67a42c4dad229ecade7d99bb5a473acd56
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-06-30 23:42:01 +0100

    previous content gone

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 // import logo from './logo.svg';
 // import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import LandingPage from './pages/LandingPage'
 import React from 'react';
-import Gallery from './pages/Gallery';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='' element= {<LandingPage/>}/>
-          <Route path='/Gallery' element= {<Gallery/>}/>
+          
+          {/* <Route path='' element= {<LandingPage/>}/>
+          <Route path='/Gallery' element= {<Gallery/>}/> */}
         </Routes>
       </BrowserRouter>
     </div>
