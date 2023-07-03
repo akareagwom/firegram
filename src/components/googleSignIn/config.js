86a96b10eea13b7101dbf3cb534b2477c8d2b404
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-07-03 22:07:46 +0100

    new google auth

--- src/components/googleSignIn/config.js
+++ src/components/googleSignIn/config.js
@@ -22,3 +22,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
