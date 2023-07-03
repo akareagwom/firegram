05ec7a62477f07641c3d3eb0e65d4997d3ee98c8
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-07-03 23:16:37 +0100

    auth with google finished

--- src/components/googleSignIn/signIn.js
+++ src/components/googleSignIn/signIn.js
@@ -2,25 +2,27 @@ import { Button } from '@mui/material';
 import {auth,provider} from './config';
 import {signInWithPopup} from "firebase/auth"
 import { useEffect, useState } from 'react';
+import Redirect from '../../pages/redirect';
+import LandingPage from '../../pages/landingPage';
 
 const SignIn = () => {
     const [value, setValue] = useState("");
 
     const handleClick=() => {
         signInWithPopup(auth,provider).then((data)=>{
-            setValue[data.user.email]
-            localStorage.setItem["email",data.user.email]
+            setValue(data.user.email)
+            localStorage.setItem("email",data.user.email)
         });
     }
 
-    useEffect(()=>{
-        setValue=(localStorage.getItem("email"))
-    },[])
+    // useEffect(()=>{
+    //     setValue=(localStorage.getItem("email"))
+    // })
 
     return ( 
         <>
-        {}
-        <Button onClick={handleClick}>SignIn With Google</Button>
+        {value?<Redirect/>:<Button onClick={handleClick}>SignIn With Google</Button>}
+        
         </>
      );
 }
