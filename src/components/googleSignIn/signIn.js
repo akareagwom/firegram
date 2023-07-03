db28d7eed87e3c0c88860a51fb85cfa85fd2e8a3
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-07-03 22:28:59 +0100

    getItem localstorage

--- src/components/googleSignIn/signIn.js
+++ src/components/googleSignIn/signIn.js
@@ -1,15 +1,25 @@
 import { Button } from '@mui/material';
 import {auth,provider} from './config';
 import {signInWithPopup} from "firebase/auth"
+import { useEffect, useState } from 'react';
 
 const SignIn = () => {
+    const [value, setValue] = useState("");
+
     const handleClick=() => {
         signInWithPopup(auth,provider).then((data)=>{
-
+            setValue[data.user.email]
+            localStorage.setItem["email",data.user.email]
         });
     }
+
+    useEffect(()=>{
+        setValue=(localStorage.getItem("email"))
+    },[])
+
     return ( 
         <>
+        {}
         <Button onClick={handleClick}>SignIn With Google</Button>
         </>
      );
