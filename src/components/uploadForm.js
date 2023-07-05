3921a7f20e65ada66f456977cbaba1f0542dd912
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-07-05 17:49:38 +0100

    setFile

--- src/components/uploadForm.js
+++ src/components/uploadForm.js
@@ -1,10 +1,19 @@
 import { FormControl } from "@mui/material";
 import React from "react";
+import {useState} from 'react'
 
 const UploadForm = () => {
+    const [file,setFile] = useState(null);
+
+    const types = ['image/png','image/jpg','image/jpeg','image/gif','image/avif']
 
     const changeHandler = (e)=>{
-        let selected
+        let selected = e.target.files[0];
+        if(selected && types.includes(selected.type)){
+            setFile(selected)
+        }else{
+            setFile(null)
+        }
     }
 
     return ( 
