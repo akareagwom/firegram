66c24eab4512a935fdfe51f4e0ba256d67ce1916
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-10-31 22:35:38 +0100

    netlify.toml

--- src/components/layout/profile.js
+++ src/components/layout/profile.js
@@ -14,15 +14,28 @@ const Profile = () => {
             <img style={{width:"600px",height:'500px'}} src="profile.png"/>
                 
         </Box>
-        <Card style={{backgroundColor: "#F2E9E4", width:"600px",height:'500px',borderRadius:"0px"}} bgcolor={'#F2E9E4'}>
-            <CardContent bgcolor={'#F2E9E4'}>
+        <Card style={{
+            backgroundColor: "#F2E9E4",
+            width:"600px",height:'500px',
+            borderRadius:"0px",
+            display:"flex",
+            flexDirection:"column",
+            justifyContent:'center'
+        }}
+            bgcolor={'#F2E9E4'}>
+            <CardContent
+            style={{
+                width:"80%"
+            }}
+             bgcolor={'#F2E9E4'}>
                 <image   height={'200px'} src="profile.png"/>
                 <Typography textAlign={'center'} variant="h5" component="h2">About Us</Typography>
                 <Typography textAlign={'center'} variant="p" component="p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis ac nibh nullam feugiat. Accumsan vestibulum varius arcu posuere orci vitae sagittis posuere vitae.</Typography>
                 <Button
                 size="large"
+                
                 bgcolor="black"
-                style={{width:"100px", backgroundColor:"black", color:"white"}} 
+                style={{ backgroundColor:"black", color:"white",paddingTop:"10px",paddingBottom:"10px",paddingLeft:"25px",paddingRight:"25px"}} 
                 >Let's Chat</Button>
             </CardContent>
 
